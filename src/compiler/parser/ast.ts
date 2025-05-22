12d364d863004e4d1aa9b3dca02714d76292b1a9
Nicolas Zozol <nzozol@gmail.com>
2025-05-22 13:54:46 +0200

    feat(dsl): designing ast nodes

---
+++ src/compiler/parser/ast.ts
@@ -0,0 +1,56 @@
+export type LiteralNode =
+  | LiteralStringNode
+  | LiteralNumberNode
+  | LiteralBooleanNode
+
+export interface LiteralStringNode {
+  type: 'literal-string'
+  value: string
+}
+
+export interface LiteralNumberNode {
+  type: 'literal-number'
+  value: number
+}
+
+export interface LiteralBooleanNode {
+  type: 'literal-boolean'
+  value: boolean
+}
+
+export interface IdentifierNode {
+  type: 'identifier'
+  value: string
+}
+
+export interface CallExpressionNode {
+  type: 'call'
+  name: string // e.g. "mappedBy", "concat", "orderBy"
+  args: ExpressionNode[] // e.g. [Identifier(tweets), Literal(id)]
+}
+
+export type ExpressionNode = IdentifierNode | LiteralNode | CallExpressionNode
+
+export interface ArgumentNode {
+  type: 'argument'
+  name: IdentifierNode
+  value: ExpressionNode
+}
+
+export interface CommandNode {
+  type: 'command'
+  command: IdentifierNode
+  args: ArgumentNode[]
+  output?: IdentifierNode
+}
+
+export interface ProgramNode {
+  type: 'program'
+  body: CommandNode[]
+}
+
+export type DslAstNode =
+  | ProgramNode
+  | CommandNode
+  | ArgumentNode
+  | ExpressionNode
