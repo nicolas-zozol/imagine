f8dcc4df7b7c1523cf3c0eaad5c9106e3d467b9e
Nicolas Zozol <nzozol@gmail.com>
2025-06-02 21:06:27 +0200

    feat(interpreter): registering three commands

--- src/compiler/handlers/register-handlers.ts
+++ src/compiler/handlers/register-handlers.ts
@@ -1,3 +1,6 @@
+import { LogHandler } from '../../homemade/utils/log.handler.js'
+import { FetchHandler } from '../../mcp/client/fetch/fetch.handler.js'
+import { FileSystemWriterHandler } from '../../mcp/client/filesystem/filesystem.handler.js'
 import { OpenAction } from '../../puppets/actions/open.js'
 import { CommandRegistry } from '../interpreter/command-registry.js'
 
@@ -15,4 +18,13 @@ export function registerAllCommandHandlers() {
 
   const open = new OpenAction()
   registry.register('@puppet/open', open)
+
+  const reader = new FetchHandler()
+  registry.register('@web/fetch', reader)
+
+  const fileWriter = new FileSystemWriterHandler()
+  registry.register('@file/write', fileWriter)
+
+  const logger = new LogHandler()
+  registry.register('@utils/log', logger)
 }
--- src/mcp/client/filesystem/filesystem.handler.ts
+++ src/mcp/client/filesystem/filesystem.handler.ts
@@ -1,3 +1,28 @@
-abstract class FileHandler {}
+import { ActionResult } from '../../../compiler/handlers/action-result.js'
+import { BaseCommandHandler } from '../../../compiler/handlers/base-command-handler.js'
+import { errorToString } from '../../../utils/error-to-string.js'
+import { FilesystemClient } from './filesystem.client.js'
 
-export class AppendToFileHandler {}
+export class FileSystemWriterHandler extends BaseCommandHandler<void> {
+  async run(
+    args: Record<string, any>,
+    context: any,
+  ): Promise<ActionResult<void>> {
+    const filePath = args.path as string
+    const content = args.content as string
+
+    return this.writeFile(filePath, content)
+  }
+
+  async writeFile(
+    filePath: string,
+    content: string,
+  ): Promise<ActionResult<void>> {
+    try {
+      await new FilesystemClient().writeFile(filePath, content)
+      return this.handleSuccess('loaded correctly')
+    } catch (e) {
+      throw this.handleError(errorToString(e))
+    }
+  }
+}
